27fb830d7c5be7ef774aa8a2539b93eaa1310994
aftabalam507 <aftabalam1996jan@gmail.com>
2024-01-23 20:28:55 +0530

    spring security and jwt configuration added

---
+++ twitter-clone-backend-api/src/main/java/com/twitter/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.twitter.config;
+
+public class JwtConstant {
+	
+	public static final String SECRET_KEY="hjsbxwjsbxjbasdqbjzxyvccvcdcwcdgfxhihsbxs";
+	public static final String JWT_HEADER="Authorization";
+}
