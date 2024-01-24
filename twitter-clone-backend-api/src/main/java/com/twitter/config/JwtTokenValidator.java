3ffac184a69ea6d7a06800362fb9857d65b2f722
aftabalam507 <aftabalam1996jan@gmail.com>
2024-01-24 20:22:33 +0530

    signup and signin api created

--- twitter-clone-backend-api/src/main/java/com/twitter/config/JwtTokenValidator.java
+++ twitter-clone-backend-api/src/main/java/com/twitter/config/JwtTokenValidator.java
@@ -22,38 +22,37 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class JwtTokenValidator extends OncePerRequestFilter {	
+public class JwtTokenValidator extends OncePerRequestFilter {
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		
-		String jwt=request.getHeader(JwtConstant.JWT_HEADER);
-		
-		if(jwt != null) {
-			jwt=jwt.substring(7);
-			
+
+		String jwt = request.getHeader(JwtConstant.JWT_HEADER);
+
+		if (jwt != null) {
+			jwt = jwt.substring(7);
+
 			try {
-				SecretKey key=Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
-				
-				Claims claims=Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
-				
+				SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+
+				Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+
 				String email = String.valueOf(claims.get("email"));
 				String authorities = String.valueOf(claims.get("authorities"));
-				
+
 				List<GrantedAuthority> auth = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
-				Authentication authentication=new UsernamePasswordAuthenticationToken(email,null, auth);
-				
+				Authentication authentication = new UsernamePasswordAuthenticationToken(email, null, auth);
+
 				SecurityContextHolder.getContext().setAuthentication(authentication);
 			} catch (Exception e) {
 				throw new BadCredentialsException("invalid token...");
 			}
-			
-			filterChain.doFilter(request, response);
-			
+
 		}
-		
+
+		filterChain.doFilter(request, response);
+
 	}
-	
-	
+
 }
